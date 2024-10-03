72d169dfbb7c46aac5d9e124bdd1f2da73314a61
Sistemas1Giga <sistemas1@gigasystem.com.ec>
2024-10-03 15:26:55 -0500

    prueba

---
+++ CapaPresentacion/Frm_clientes.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace CapaPresentacion
+{
+    partial class Frm_clientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.ShowCheckMargin = true;
+            contextMenuStrip1.ShowImageMargin = false;
+            contextMenuStrip1.Size = new Size(61, 4);
+            contextMenuStrip1.Text = "1";
+            // 
+            // Frm_clientes
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(881, 489);
+            Name = "Frm_clientes";
+            Text = "Frm_clientes";
+            Load += Frm_clientes_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ CapaPresentacion/Frm_clientes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class Frm_clientes : Form
+    {
+        public Frm_clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_clientes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
