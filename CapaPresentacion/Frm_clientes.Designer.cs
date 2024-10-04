215613c6f77cc0bca30c5b87b7b8d740763b2cd5
fabianaj88 <fabianajp88@gmail.com>
2024-10-04 12:20:44 -0500

    c

--- CapaPresentacion/Frm_clientes.Designer.cs
+++ CapaPresentacion/Frm_clientes.Designer.cs
@@ -43,24 +43,20 @@
             label5 = new Label();
             label4 = new Label();
             label3 = new Label();
-            label2 = new Label();
-            textBox3 = new TextBox();
-            textBox2 = new TextBox();
-            textBox1 = new TextBox();
             txt_codigo = new TextBox();
-            label1 = new Label();
-            pictureBox3 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            btn_buscar = new Button();
-            btn_eliminar = new Button();
-            btn_editar = new Button();
-            btn_grabar = new Button();
-            btn_nuevo = new Button();
-            pictureBox1 = new PictureBox();
-            tabPage2 = new TabPage();
-            tabPage3 = new TabPage();
-            tabPage4 = new TabPage();
-            Locales.SuspendLayout();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            txt_codigo = new TextBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            txt_codigo = new TextBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            //tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
@@ -120,186 +116,69 @@
             tabPage1.Size = new Size(849, 437);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Registro";
+            tabPage1.Click += tabPage1_Click;
             // 
-            // comboBox1
+            // tabPage2
             // 
-            comboBox1.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(127, 384);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(203, 24);
-            comboBox1.TabIndex = 23;
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(849, 437);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Listado";
+            tabPage2.UseVisualStyleBackColor = true;
             // 
-            // label7
+            // pictureBox1
             // 
-            label7.AutoSize = true;
-            label7.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            label7.Location = new Point(40, 392);
-            label7.Name = "label7";
-            label7.Size = new Size(59, 16);
-            label7.TabIndex = 22;
-            label7.Text = "Genero:";
+            pictureBox1.BackColor = Color.LightSkyBlue;
+            pictureBox1.Location = new Point(17, 42);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(805, 37);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
-            // radioButton3
+            // btn_nuevo
             // 
-            radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(253, 94);
-            radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(77, 19);
-            radioButton3.TabIndex = 21;
-            radioButton3.TabStop = true;
-            radioButton3.Text = "Pasaporte";
-            radioButton3.UseVisualStyleBackColor = true;
+            btn_nuevo.Image = (Image)resources.GetObject("btn_nuevo.Image");
+            btn_nuevo.Location = new Point(55, 48);
+            btn_nuevo.Name = "btn_nuevo";
+            btn_nuevo.Size = new Size(75, 23);
+            btn_nuevo.TabIndex = 1;
+            btn_nuevo.Text = "Nuevo";
+            btn_nuevo.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // btn_grabar
             // 
-            radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(164, 94);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(45, 19);
-            radioButton2.TabIndex = 20;
-            radioButton2.TabStop = true;
-            radioButton2.Text = "Ruc";
-            radioButton2.UseVisualStyleBackColor = true;
+            btn_grabar.Image = (Image)resources.GetObject("btn_grabar.Image");
+            btn_grabar.Location = new Point(151, 48);
+            btn_grabar.Name = "btn_grabar";
+            btn_grabar.Size = new Size(75, 23);
+            btn_grabar.TabIndex = 2;
+            btn_grabar.Text = "Grabar";
+            btn_grabar.UseVisualStyleBackColor = true;
             // 
-            // radioButton1
+            // btn_editar
             // 
-            radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(55, 94);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(62, 19);
-            radioButton1.TabIndex = 19;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "Cedula";
-            radioButton1.UseVisualStyleBackColor = true;
+            btn_editar.Image = (Image)resources.GetObject("btn_editar.Image");
+            btn_editar.Location = new Point(253, 48);
+            btn_editar.Name = "btn_editar";
+            btn_editar.Size = new Size(75, 23);
+            btn_editar.TabIndex = 3;
+            btn_editar.Text = "Editar";
+            btn_editar.UseVisualStyleBackColor = true;
             // 
-            // label6
+            // btn_eliminar
             // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            label6.Location = new Point(40, 348);
-            label6.Name = "label6";
-            label6.Size = new Size(55, 16);
-            label6.TabIndex = 18;
-            label6.Text = "Correo:";
-            // 
-            // textBox4
-            // 
-            textBox4.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox4.Location = new Point(127, 341);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(203, 23);
-            textBox4.TabIndex = 17;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(42, 300);
-            label5.Name = "label5";
-            label5.Size = new Size(65, 16);
-            label5.TabIndex = 16;
-            label5.Text = "Telefono:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(42, 253);
-            label4.Name = "label4";
-            label4.Size = new Size(70, 16);
-            label4.TabIndex = 15;
-            label4.Text = "Direccion:";
-            // 
-            // label3
-            // 
-            label3.Font = new Font("Arial", 15F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(379, 12);
-            label3.Name = "label3";
-            label3.Size = new Size(100, 20);
-            label3.TabIndex = 14;
-            label3.Text = "Clientes";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.Transparent;
-            label2.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(40, 209);
-            label2.Name = "label2";
-            label2.Size = new Size(55, 16);
-            label2.TabIndex = 13;
-            label2.Text = "Cliente:";
-            label2.Click += label2_Click;
-            // 
-            // textBox3
-            // 
-            textBox3.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox3.Location = new Point(127, 293);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(203, 23);
-            textBox3.TabIndex = 12;
-            // 
-            // textBox2
-            // 
-            textBox2.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox2.Location = new Point(125, 246);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(388, 23);
-            textBox2.TabIndex = 11;
-            // 
-            // textBox1
-            // 
-            textBox1.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(125, 202);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(388, 23);
-            textBox1.TabIndex = 10;
-            // 
-            // txt_codigo
-            // 
-            txt_codigo.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            txt_codigo.Location = new Point(125, 166);
-            txt_codigo.Name = "txt_codigo";
-            txt_codigo.Size = new Size(203, 23);
-            txt_codigo.TabIndex = 9;
-            txt_codigo.TextChanged += txt_codigo_TextChanged;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = Color.Transparent;
-            label1.BorderStyle = BorderStyle.Fixed3D;
-            label1.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(40, 166);
-            label1.Name = "label1";
-            label1.Size = new Size(58, 18);
-            label1.TabIndex = 8;
-            label1.Text = "Código:";
+            btn_eliminar.Image = (Image)resources.GetObject("btn_eliminar.Image");
+            btn_eliminar.Location = new Point(353, 48);
+            btn_eliminar.Name = "btn_eliminar";
+            btn_eliminar.Size = new Size(75, 23);
+            btn_eliminar.TabIndex = 4;
+            btn_eliminar.Text = "Eliminar";
+            btn_eliminar.UseVisualStyleBackColor = true;
             // 
             // pictureBox3
             // 
-            pictureBox3.BackColor = Color.Gainsboro;
-            pictureBox3.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox3.Location = new Point(17, 139);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(805, 292);
-            pictureBox3.TabIndex = 7;
-            pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.BackColor = Color.Gainsboro;
-            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox2.Location = new Point(17, 85);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(805, 38);
-            pictureBox2.TabIndex = 6;
-            pictureBox2.TabStop = false;
-            // 
-            // btn_buscar
-            // 
-            btn_buscar.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
             btn_buscar.Image = (Image)resources.GetObject("btn_buscar.Image");
             btn_buscar.Location = new Point(451, 48);
             btn_buscar.Name = "btn_buscar";
@@ -319,77 +198,63 @@
             btn_eliminar.Text = "Eliminar";
             btn_eliminar.UseVisualStyleBackColor = true;
             // 
-            // btn_editar
+            // pictureBox3
             // 
-            btn_editar.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            btn_editar.Image = (Image)resources.GetObject("btn_editar.Image");
-            btn_editar.Location = new Point(253, 48);
-            btn_editar.Name = "btn_editar";
-            btn_editar.Size = new Size(75, 23);
-            btn_editar.TabIndex = 3;
-            btn_editar.Text = "Editar";
-            btn_editar.UseVisualStyleBackColor = true;
+            pictureBox3.BackColor = Color.WhiteSmoke;
+            pictureBox3.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox3.Location = new Point(17, 139);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(805, 292);
+            pictureBox3.TabIndex = 7;
+            pictureBox3.TabStop = false;
             // 
-            // btn_grabar
+            // label1
             // 
-            btn_grabar.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            btn_grabar.Image = (Image)resources.GetObject("btn_grabar.Image");
-            btn_grabar.Location = new Point(151, 48);
-            btn_grabar.Name = "btn_grabar";
-            btn_grabar.Size = new Size(75, 23);
-            btn_grabar.TabIndex = 2;
-            btn_grabar.Text = "Grabar";
-            btn_grabar.UseVisualStyleBackColor = true;
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 166);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Código";
             // 
-            // btn_nuevo
+            // txt_codigo
             // 
-            btn_nuevo.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            btn_nuevo.Image = (Image)resources.GetObject("btn_nuevo.Image");
-            btn_nuevo.Location = new Point(55, 48);
-            btn_nuevo.Name = "btn_nuevo";
-            btn_nuevo.Size = new Size(75, 23);
-            btn_nuevo.TabIndex = 1;
-            btn_nuevo.Text = "Nuevo";
-            btn_nuevo.UseVisualStyleBackColor = true;
+            txt_codigo.Location = new Point(106, 166);
+            txt_codigo.Name = "txt_codigo";
+            txt_codigo.Size = new Size(203, 23);
+            txt_codigo.TabIndex = 9;
+            txt_codigo.TextChanged += txt_codigo_TextChanged;
             // 
-            // pictureBox1
+            // textBox1
             // 
-            pictureBox1.BackColor = Color.LightSkyBlue;
-            pictureBox1.Location = new Point(17, 42);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(805, 37);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            textBox1.Location = new Point(106, 206);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(388, 23);
+            textBox1.TabIndex = 10;
             // 
-            // tabPage2
+            // textBox2
             // 
-            tabPage2.Location = new Point(4, 24);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(849, 437);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "Listado";
-            tabPage2.UseVisualStyleBackColor = true;
+            textBox2.Location = new Point(106, 250);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(388, 23);
+            textBox2.TabIndex = 11;
             // 
-            // tabPage3
+            // textBox3
             // 
-            tabPage3.Location = new Point(4, 24);
-            tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(849, 437);
-            tabPage3.TabIndex = 2;
-            tabPage3.Text = "tabPage3";
-            tabPage3.UseVisualStyleBackColor = true;
+            textBox3.Location = new Point(106, 297);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(203, 23);
+            textBox3.TabIndex = 12;
             // 
-            // tabPage4
+            // label2
             // 
-            tabPage4.Location = new Point(4, 24);
-            tabPage4.Name = "tabPage4";
-            tabPage4.Padding = new Padding(3);
-            tabPage4.Size = new Size(849, 437);
-            tabPage4.TabIndex = 3;
-            tabPage4.Text = "tabPage4";
-            tabPage4.UseVisualStyleBackColor = true;
+            label2.AutoSize = true;
+            label2.Location = new Point(40, 209);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 13;
+            label2.Text = "Cliente";
+            label2.Click += label2_Click;
             // 
             // Frm_clientes
             // 
