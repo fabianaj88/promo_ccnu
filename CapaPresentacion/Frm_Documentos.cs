9072641dbc252a4292091399bd74a99d732a573f
trabajoInte92 <trabajointegracurri92@gmail.com>
2024-10-03 22:33:13 -0500

    cambioDise

---
+++ CapaPresentacion/Frm_Documentos.cs
@@ -0,0 +1,58 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class Frm_Documentos : Form
+    {
+        public Frm_Documentos()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia de la clase Conexion
+            Conexion conexion = new Conexion();
+
+            try
+            {
+                // Abrir la conexión
+                SqlConnection con = conexion.AbrirConexion();
+                MessageBox.Show("Conexión abierta correctamente.");
+
+                // Realizar aquí las operaciones que necesites con la conexión...
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier error
+                MessageBox.Show("Error al abrir la conexión: " + ex.Message);
+            }
+            finally
+            {
+                // Cerrar la conexión
+                conexion.CerrarConexion();
+                MessageBox.Show("Conexión cerrada.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -11,7 +11,7 @@ namespace CapaPresentacion
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new P_Documentos());
+            Application.Run(new Frm_Documentos());
         }
     }
 }
